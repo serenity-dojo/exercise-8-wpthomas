bf074b1ff279106c3c259e43118e35fa7c8ab983
wpthomas <31428486+wpthomas@users.noreply.github.com>
2024-03-19 09:21:02 -0400

    Made changes to WhenWorkingWithSets tests so they pass.

--- src/test/java/com/serenitydojo/WhenWorkingWithSets.java
+++ src/test/java/com/serenitydojo/WhenWorkingWithSets.java
@@ -27,7 +27,7 @@ public class WhenWorkingWithSets {
     // We can create a set containing a specified list of values")
     @Test
     public void creatingASetOfValues() {
-        Set<String> colors = null;
+        Set<String> colors = new HashSet<>(Set.of("red", "green","blue"));
 
         // TODO: Create a set of Strings containing the values "red", "green" and "blue"
         //  using the Set.of() method
@@ -45,7 +45,7 @@ public class WhenWorkingWithSets {
         colors.add("yellow");
         colors.add("red");
 
-        int expectedSize = 0;
+        int expectedSize = 4;
 
         assertThat(colors.size()).isEqualTo(expectedSize);
     }
@@ -56,14 +56,14 @@ public class WhenWorkingWithSets {
         Set<String> colors = new HashSet<>();
 
         // TODO: Change this code so that it correctly determines whether the set is empty
-        Boolean startedEmpty = null;
+        Boolean startedEmpty = colors.isEmpty();
 
         colors.add("red");
         colors.add("green");
         colors.add("blue");
 
         // TODO: Change this code so that it correctly determines whether the set now empty or not
-        Boolean endedUpEmpty = null;
+        Boolean endedUpEmpty = colors.isEmpty();
 
         assertThat(startedEmpty).isTrue();
         assertThat(endedUpEmpty).isFalse();
